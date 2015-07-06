202c080a3bd39154d59997ddede41215d0131d9a
Anurag Peshne <anurag.peshne@gmail.com>
2015-07-06 15:24:18 +0530

    added support for pomoDaemon in tmux files

---
+++ development.sh
@@ -0,0 +1,9 @@
+pomoDaemon
+tmux new-session -s development -n editor -d
+tmux send-keys -t development 'cd ~/code' C-m
+tmux send-keys -t development 'vim' C-m
+tmux send-keys -t development ':e ./' C-m
+#tmux split-window -h -t development
+#tmux new-window -n console -t development
+tmux select-window -t development:0
+tmux attach -t development
