90ea2b80c85b69510334fd36be122e128689ae9e
Anurag Peshne <anurag.peshne@gmail.com>
2018-09-24 19:14:50 -0700

    fixed installation script, added option for spacemacs

--- install.sh
+++ install.sh
@@ -13,11 +13,11 @@ GREEN='\033[1;32m'
 # actual installation begins
 
 echo -n "Installing bash config..."
-ln -s DIR/.bash.rc ~/.bash.rc
+ln -s $DIR/.bash.rc ~/.bash.rc
 echo -e "${GREEN} installed${NC}"
 
 echo "Installing vim..."
-ln -s DIR/.vimrc ~/.vimrc
+ln -s $DIR/.vimrc ~/.vimrc
 if [ ! -d "~/.vim" ]; then
   mkdir ~/.vim
   mkdir ~/.vim/colors
@@ -28,7 +28,7 @@ if [ ! -d "~/.vim/bundle" ]; then
   mkdir ~/.vim/bundle
   echo "~/.vim/bundle directory created"
 fi
-command -v git > /dev/null 2>&1 || { 
+command -v git > /dev/null 2>&1 || {
   echo "need git; install git and try again...";
   exit 1;
 }
@@ -44,9 +44,22 @@ vim +PluginInstall +qall
 echo -e "${GREEN} Vim plugins installed${NC}"
 
 echo -n "Creating soft link for tmux config..."
-ln -s DIR/.tmux.config ~/.tmux.config
+ln -s $DIR/.tmux.config ~/.tmux.config
 echo -e "${GREEN} done${NC}"
 
-echo -n "Creating soft link for '.emacs.d'..."
-ln -s DIR/.emacs.d ~/.emacs.d
-echo -e "${GREEN} done${NC}"
+# install spacemacs or custom emacs.d
+while true; do
+  read -p "Install (S)pacemacs or (C)ustom .emacs.d (S/C)?" sc
+  case $sc in
+    [Ss]* ) git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d;
+            echo "Creating softlink for .spacemacs..."
+            ln -s $DIR/.spacemacs ~/.spacemacs
+            echo -e "${GREEN} done${NC}"
+            break;;
+    [Cc]* ) echo -n "Creating soft link for '.emacs.d'..."
+            ln -s $DIR/.emacs.d ~/.emacs.d
+            echo -e "${GREEN} done${NC}"
+            break;;
+    * ) echo "Please answer S or C.";;
+  esac
+done
