37a8001490bc584e3638737cb222e5c6eccff6d5
Anurag Peshne <anpeshne@microsoft.com>
2018-09-28 17:29:48 -0700

    powershell install script init

---
+++ install.ps1
@@ -0,0 +1,16 @@
+﻿function Test-IsAdmin {
+    $identity = [Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()
+    $identity.IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
+}
+
+if (!(Test-IsAdmin)){
+    throw "Please run this script with admin priviliges"
+}
+
+$PWD=(Get-Item -Path ".\").FullName
+
+Write-Output "Assuming $PWD as root directory for dot_files repository"
+
+Write-Output "Linking .vimrc" 
+cmd /c mklink $HOME\_vimrc $PWD\.vimrc
+Write-Output "Done"
